1b4bf96b1791e9de9c9c6176ad4f356f990cd0be
Eleven <ahmetyasinoguzcan@gmail.com>
2026-09-10 21:55:23 +0300

    refactor(ig07): move cache implementation into support package

--- brain_eleven/support/__init__.py
+++ brain_eleven/support/__init__.py
@@ -14,22 +14,23 @@ from brain_eleven.support.logging import (
     logger,
     setup_logging,
 )
+from brain_eleven.support.cache import (
+    CacheManager,
+    DiskCache,
+    LRUCache,
+    REDIS_AVAILABLE,
+)
 
 
 _summarizer = load_legacy_module("summarizer", "summarizer.py")
 _anomaly_detector = load_legacy_module("anomaly_detector", "anomaly_detector.py")
-_cache_manager = load_legacy_module("cache_manager", "cache_manager.py")
+_cache_manager_adapter = load_legacy_module("cache_manager", "cache_manager.py")
 
 MemorySummarizer = _summarizer.MemorySummarizer
 tokenize = _summarizer.tokenize
 jaccard_similarity = _summarizer.jaccard_similarity
 
 AnomalyDetector = _anomaly_detector.AnomalyDetector
-
-LRUCache = _cache_manager.LRUCache
-DiskCache = _cache_manager.DiskCache
-CacheManager = _cache_manager.CacheManager
-REDIS_AVAILABLE = _cache_manager.REDIS_AVAILABLE
 
 __all__ = [
     "AnomalyDetector",
